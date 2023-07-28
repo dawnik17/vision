0f51d8ad79f6b4754fbe9c95786ff1e8572b18e1
dawnik17 <dawnik17@gmail.com>
2023-07-28 14:47:40 +0530

    GRAD CAM

--- examples/cifar10/README.md
+++ examples/cifar10/README.md
@@ -16,90 +16,90 @@ The repository follows a specific directory structure:
 
 ### src/models/resnet.py
 
-Our model is a modified version of Resnet18 with reduced channel size. It achieves an impressive accuracy of **73%** in less than 15 Epochs, utilizing a total of **38,874 parameters**. The `model.py` file contains the implementation of the neural network model architecture. The current architecture has been chosen to align with the project objectives. Here is the model architecture:
+The `model.py` file contains the implementation of the neural network model architecture. The current architecture has been chosen to align with the project objectives. Here is the model architecture:
 
 ```
 ----------------------------------------------------------------
         Layer (type)               Output Shape         Param #
 ================================================================
-            Conv2d-1           [-1, 16, 32, 32]             432
-       BatchNorm2d-2           [-1, 16, 32, 32]              32
-              ReLU-3           [-1, 16, 32, 32]               0
-           Dropout-4           [-1, 16, 32, 32]               0
-            Conv2d-5           [-1, 16, 32, 32]           2,304
-       BatchNorm2d-6           [-1, 16, 32, 32]              32
-              ReLU-7           [-1, 16, 32, 32]               0
-           Dropout-8           [-1, 16, 32, 32]               0
-            Conv2d-9           [-1, 16, 32, 32]           2,304
-      BatchNorm2d-10           [-1, 16, 32, 32]              32
-       BasicBlock-11           [-1, 16, 32, 32]               0
-           Conv2d-12           [-1, 16, 32, 32]           2,304
-      BatchNorm2d-13           [-1, 16, 32, 32]              32
-             ReLU-14           [-1, 16, 32, 32]               0
-          Dropout-15           [-1, 16, 32, 32]               0
-           Conv2d-16           [-1, 16, 32, 32]           2,304
-      BatchNorm2d-17           [-1, 16, 32, 32]              32
-       BasicBlock-18           [-1, 16, 32, 32]               0
-           Conv2d-19           [-1, 16, 16, 16]           2,304
-      BatchNorm2d-20           [-1, 16, 16, 16]              32
-             ReLU-21           [-1, 16, 16, 16]               0
-          Dropout-22           [-1, 16, 16, 16]               0
-           Conv2d-23           [-1, 16, 16, 16]           2,304
-      BatchNorm2d-24           [-1, 16, 16, 16]              32
-           Conv2d-25           [-1, 16, 16, 16]             256
-      BatchNorm2d-26           [-1, 16, 16, 16]              32
-       BasicBlock-27           [-1, 16, 16, 16]               0
-           Conv2d-28           [-1, 16, 16, 16]           2,304
-      BatchNorm2d-29           [-1, 16, 16, 16]              32
-             ReLU-30           [-1, 16, 16, 16]               0
-          Dropout-31           [-1, 16, 16, 16]               0
-           Conv2d-32           [-1, 16, 16, 16]           2,304
-      BatchNorm2d-33           [-1, 16, 16, 16]              32
-       BasicBlock-34           [-1, 16, 16, 16]               0
-           Conv2d-35             [-1, 16, 8, 8]           2,304
-      BatchNorm2d-36             [-1, 16, 8, 8]              32
-             ReLU-37             [-1, 16, 8, 8]               0
-          Dropout-38             [-1, 16, 8, 8]               0
-           Conv2d-39             [-1, 16, 8, 8]           2,304
-      BatchNorm2d-40             [-1, 16, 8, 8]              32
-           Conv2d-41             [-1, 16, 8, 8]             256
-      BatchNorm2d-42             [-1, 16, 8, 8]              32
-       BasicBlock-43             [-1, 16, 8, 8]               0
-           Conv2d-44             [-1, 16, 8, 8]           2,304
-      BatchNorm2d-45             [-1, 16, 8, 8]              32
-             ReLU-46             [-1, 16, 8, 8]               0
-          Dropout-47             [-1, 16, 8, 8]               0
-           Conv2d-48             [-1, 16, 8, 8]           2,304
-      BatchNorm2d-49             [-1, 16, 8, 8]              32
-       BasicBlock-50             [-1, 16, 8, 8]               0
-           Conv2d-51             [-1, 16, 4, 4]           2,304
-      BatchNorm2d-52             [-1, 16, 4, 4]              32
-             ReLU-53             [-1, 16, 4, 4]               0
-          Dropout-54             [-1, 16, 4, 4]               0
-           Conv2d-55             [-1, 16, 4, 4]           2,304
-      BatchNorm2d-56             [-1, 16, 4, 4]              32
-           Conv2d-57             [-1, 16, 4, 4]             256
-      BatchNorm2d-58             [-1, 16, 4, 4]              32
-       BasicBlock-59             [-1, 16, 4, 4]               0
-           Conv2d-60             [-1, 16, 4, 4]           2,304
-      BatchNorm2d-61             [-1, 16, 4, 4]              32
-             ReLU-62             [-1, 16, 4, 4]               0
-          Dropout-63             [-1, 16, 4, 4]               0
-           Conv2d-64             [-1, 16, 4, 4]           2,304
-      BatchNorm2d-65             [-1, 16, 4, 4]              32
-       BasicBlock-66             [-1, 16, 4, 4]               0
-           Conv2d-67             [-1, 10, 4, 4]             170
+            Conv2d-1           [-1, 64, 32, 32]           1,728
+       BatchNorm2d-2           [-1, 64, 32, 32]             128
+              ReLU-3           [-1, 64, 32, 32]               0
+           Dropout-4           [-1, 64, 32, 32]               0
+            Conv2d-5           [-1, 64, 32, 32]          36,864
+       BatchNorm2d-6           [-1, 64, 32, 32]             128
+              ReLU-7           [-1, 64, 32, 32]               0
+           Dropout-8           [-1, 64, 32, 32]               0
+            Conv2d-9           [-1, 64, 32, 32]          36,864
+      BatchNorm2d-10           [-1, 64, 32, 32]             128
+       BasicBlock-11           [-1, 64, 32, 32]               0
+           Conv2d-12           [-1, 64, 32, 32]          36,864
+      BatchNorm2d-13           [-1, 64, 32, 32]             128
+             ReLU-14           [-1, 64, 32, 32]               0
+          Dropout-15           [-1, 64, 32, 32]               0
+           Conv2d-16           [-1, 64, 32, 32]          36,864
+      BatchNorm2d-17           [-1, 64, 32, 32]             128
+       BasicBlock-18           [-1, 64, 32, 32]               0
+           Conv2d-19          [-1, 128, 16, 16]          73,728
+      BatchNorm2d-20          [-1, 128, 16, 16]             256
+             ReLU-21          [-1, 128, 16, 16]               0
+          Dropout-22          [-1, 128, 16, 16]               0
+           Conv2d-23          [-1, 128, 16, 16]         147,456
+      BatchNorm2d-24          [-1, 128, 16, 16]             256
+           Conv2d-25          [-1, 128, 16, 16]           8,192
+      BatchNorm2d-26          [-1, 128, 16, 16]             256
+       BasicBlock-27          [-1, 128, 16, 16]               0
+           Conv2d-28          [-1, 128, 16, 16]         147,456
+      BatchNorm2d-29          [-1, 128, 16, 16]             256
+             ReLU-30          [-1, 128, 16, 16]               0
+          Dropout-31          [-1, 128, 16, 16]               0
+           Conv2d-32          [-1, 128, 16, 16]         147,456
+      BatchNorm2d-33          [-1, 128, 16, 16]             256
+       BasicBlock-34          [-1, 128, 16, 16]               0
+           Conv2d-35            [-1, 256, 8, 8]         294,912
+      BatchNorm2d-36            [-1, 256, 8, 8]             512
+             ReLU-37            [-1, 256, 8, 8]               0
+          Dropout-38            [-1, 256, 8, 8]               0
+           Conv2d-39            [-1, 256, 8, 8]         589,824
+      BatchNorm2d-40            [-1, 256, 8, 8]             512
+           Conv2d-41            [-1, 256, 8, 8]          32,768
+      BatchNorm2d-42            [-1, 256, 8, 8]             512
+       BasicBlock-43            [-1, 256, 8, 8]               0
+           Conv2d-44            [-1, 256, 8, 8]         589,824
+      BatchNorm2d-45            [-1, 256, 8, 8]             512
+             ReLU-46            [-1, 256, 8, 8]               0
+          Dropout-47            [-1, 256, 8, 8]               0
+           Conv2d-48            [-1, 256, 8, 8]         589,824
+      BatchNorm2d-49            [-1, 256, 8, 8]             512
+       BasicBlock-50            [-1, 256, 8, 8]               0
+           Conv2d-51            [-1, 512, 4, 4]       1,179,648
+      BatchNorm2d-52            [-1, 512, 4, 4]           1,024
+             ReLU-53            [-1, 512, 4, 4]               0
+          Dropout-54            [-1, 512, 4, 4]               0
+           Conv2d-55            [-1, 512, 4, 4]       2,359,296
+      BatchNorm2d-56            [-1, 512, 4, 4]           1,024
+           Conv2d-57            [-1, 512, 4, 4]         131,072
+      BatchNorm2d-58            [-1, 512, 4, 4]           1,024
+       BasicBlock-59            [-1, 512, 4, 4]               0
+           Conv2d-60            [-1, 512, 4, 4]       2,359,296
+      BatchNorm2d-61            [-1, 512, 4, 4]           1,024
+             ReLU-62            [-1, 512, 4, 4]               0
+          Dropout-63            [-1, 512, 4, 4]               0
+           Conv2d-64            [-1, 512, 4, 4]       2,359,296
+      BatchNorm2d-65            [-1, 512, 4, 4]           1,024
+       BasicBlock-66            [-1, 512, 4, 4]               0
+           Conv2d-67             [-1, 10, 4, 4]           5,130
         AvgPool2d-68             [-1, 10, 1, 1]               0
           Flatten-69                   [-1, 10]               0
 ================================================================
-Total params: 38,874
-Trainable params: 38,874
+Total params: 11,173,962
+Trainable params: 11,173,962
 Non-trainable params: 0
 ----------------------------------------------------------------
 Input size (MB): 0.01
-Forward/backward pass size (MB): 2.91
-Params size (MB): 0.15
-Estimated Total Size (MB): 3.07
+Forward/backward pass size (MB): 16.00
+Params size (MB): 42.63
+Estimated Total Size (MB): 58.64
 ----------------------------------------------------------------
 ```
 
@@ -112,14 +112,21 @@ The `utils.py` file includes helper functions for various tasks such as obtainin
 The main file for this project is the Jupyter Notebook file, `cifar.ipynb`. It encompasses the training and evaluation of the model.
 
 ## Results
+Class Activation Map through 25 Epochs: A Demonstration of How the Model Learns.
+
+Ship | Airplane | Bird | Dog | Frog |
+:---:|:--------:|:----:|:---:|:----:|
+<img src="images/cam/ship.gif"  width="100" height="100">  |  <img src="images/cam/airplane.gif"  width="100" height="100"> |  <img src="images/cam/bird.gif"  width="100" height="100"> |  <img src="images/cam/dog.gif"  width="100" height="100"> |  <img src="images/cam/frog.gif"  width="100" height="100">
 
 Our trained model has demonstrated exceptional performance, achieving high accuracy even on challenging images from the CIFAR-10 dataset. Here are some examples of correctly classified images:
 
 ![Correctly Classified Images](images/correct_classification.png)
+![Correctly Classified Images](images/correct_classification_cam.png)
 
 However, it is important to note that there are instances where our model struggles to make accurate predictions. The following images present significant challenges for comprehension:
 
 ![Incorrectly Classified Images](images/incorrect_classification.png)
+![Incorrectly Classified Images](images/incorrect_classification_cam.png)
 
 These instances serve as a reminder that even state-of-the-art models have limitations when confronted with highly ambiguous or complex images.
 
--- src/utils.py
+++ src/utils.py
@@ -1,122 +1,287 @@
 import math
-
+from typing import List
+import os
+import imageio
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn.functional as F
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from tqdm import tqdm
 
-train_losses = []
-test_losses = []
-train_acc = []
-test_acc = []
 
+class Train:
+    def __init__(self, model, trainloader, optimizer, criterion, scheduler=None):
+        self.device = self.get_device()
+        self.model = model.to(self.device)
 
-def get_device():
-    return "cuda" if torch.cuda.is_available() else "cpu"
-
-
-def train(model, device, train_loader, optimizer, criterion, scheduler=None):
-    model.train()
-    pbar = tqdm(train_loader)
-
-    train_loss = 0
-    correct = 0
-    processed = 0
-
-    for batch_idx, (data, target) in enumerate(pbar):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-
-        # Predict
-        pred = model(data)
-
-        # Calculate loss
-        loss = criterion(pred, target)
-        train_loss += loss.item()
-
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-
-        if scheduler is not None:
-            scheduler.step()
-
-        correct += (pred.argmax(-1) == target).sum().item()
-        processed += len(data)
-
-        pbar.set_description(
-            desc=f"Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}"
+        self.trainloader = trainloader
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.scheduler = (
+            scheduler if scheduler.__class__.__name__.lower() == "onecyclelr" else None
         )
 
-    train_acc.append(100 * correct / processed)
-    train_losses.append(train_loss / len(train_loader))
+        self.train_losses = []
+        self.train_accuracy = []
 
+    def get_device(self):
+        return "cuda" if torch.cuda.is_available() else "cpu"
 
-def test(model, device, test_loader, criterion):
-    model.eval()
+    def __call__(self):
+        self.model.train()
 
-    test_loss = 0
-    correct = 0
+        pbar = tqdm(self.trainloader)
+        train_loss = 0
+        correct = 0
+        processed = 0
 
-    with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(test_loader):
-            data, target = data.to(device), target.to(device)
+        for batch_idx, (data, target) in enumerate(pbar):
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
 
-            pred = model(data)
-            test_loss += criterion(
-                pred, target, reduction="sum"
-            ).item()  # sum up batch loss
+            # Predict
+            pred = self.model(data)
+
+            # Calculate loss
+            loss = self.criterion(pred, target)
+            train_loss += loss.item()
+
+            # Backpropagation
+            loss.backward()
+            self.optimizer.step()
+
+            if self.scheduler is not None:
+                self.scheduler.step()
 
             correct += (pred.argmax(-1) == target).sum().item()
+            processed += len(target)
 
-    test_loss /= len(test_loader.dataset)
-    test_acc.append(100.0 * correct / len(test_loader.dataset))
-    test_losses.append(test_loss)
+            # Display progress
+            pbar.set_description(
+                desc=f"Train: Loss={train_loss / (batch_idx + 1):0.4f} Batch_id={batch_idx} Accuracy={100 * correct / processed:0.2f}"
+            )
 
-    print(
-        "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n".format(
-            test_loss,
-            correct,
-            len(test_loader.dataset),
-            100.0 * correct / len(test_loader.dataset),
+        # Record training statistics
+        self.train_losses.append(train_loss / len(self.trainloader))
+        self.train_accuracy.append(100 * correct / processed)
+
+    def plot_stats(self):
+        fig, axs = plt.subplots(2, 1, figsize=(10, 8))
+        axs[0].plot(self.train_losses, label="Training Loss")
+        axs[0].set_title("Training Loss")
+        axs[0].set_xlabel("Epoch")
+        axs[0].set_ylabel("Loss")
+        axs[0].legend()
+
+        axs[1].plot(self.train_accuracy, label="Training Accuracy")
+        axs[1].set_title("Training Accuracy")
+        axs[1].set_xlabel("Epoch")
+        axs[1].set_ylabel("Accuracy (%)")
+        axs[1].legend()
+
+        plt.tight_layout()
+        plt.show()
+
+
+class Test:
+    def __init__(self, model, testloader, criterion):
+        self.device = self.get_device()
+        self.model = model.to(self.device)
+
+        self.testloader = testloader
+        self.criterion = criterion
+
+        self.test_losses = []
+        self.test_accuracy = []
+
+    def get_device(self):
+        return "cuda" if torch.cuda.is_available() else "cpu"
+
+    def __call__(self):
+        self.model.eval()
+        test_loss = 0
+        correct = 0
+        total_samples = len(self.testloader.dataset)
+
+        with torch.no_grad():
+            for data, target in self.testloader:
+                data, target = data.to(self.device), target.to(self.device)
+                pred = self.model(data)
+
+                test_loss += self.criterion(pred, target, reduction="sum").item()
+                correct += (pred.argmax(-1) == target).sum().item()
+
+        # Calculate and record test statistics
+        test_loss /= total_samples
+
+        self.test_losses.append(test_loss)
+        self.test_accuracy.append(100.0 * correct / total_samples)
+
+        print(
+            "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n".format(
+                test_loss, correct, total_samples, 100.0 * correct / total_samples
+            )
         )
-    )
-    return test_loss
+
+    def plot_stats(self):
+        fig, axs = plt.subplots(2, 1, figsize=(10, 8))
+        axs[0].plot(self.test_losses, label="Testing Loss")
+        axs[0].set_title("Testing Loss")
+        axs[0].set_xlabel("Epoch")
+        axs[0].set_ylabel("Loss")
+        axs[0].legend()
+
+        axs[1].plot(self.test_accuracy, label="Testing Accuracy")
+        axs[1].set_title("Testing Accuracy")
+        axs[1].set_xlabel("Epoch")
+        axs[1].set_ylabel("Accuracy (%)")
+        axs[1].legend()
+
+        plt.tight_layout()
+        plt.show()
 
 
-def plot_stats():
-    fig, axs = plt.subplots(2, 2, figsize=(15, 10))
-    axs[0, 0].plot(train_losses)
-    axs[0, 0].set_title("Training Loss")
-    axs[1, 0].plot(train_acc)
-    axs[1, 0].set_title("Training Accuracy")
-    axs[0, 1].plot(test_losses)
-    axs[0, 1].set_title("Test Loss")
-    axs[1, 1].plot(test_acc)
-    axs[1, 1].set_title("Test Accuracy")
+class TrainTest:
+    def __init__(
+        self,
+        model,
+        trainloader,
+        testloader,
+        optimizer,
+        criterion,
+        scheduler,
+        target_layers=None,
+    ):
+        self.train = Train(model, trainloader, optimizer, criterion, scheduler)
+        self.test = Test(model, testloader, criterion)
+
+        self.scheduler_per_epoch = scheduler.__class__.__name__.lower() != "onecyclelr"
+        self.scheduler = scheduler
+
+        self.cam = None
+        self.target_layers = target_layers
+        self.cam_results = dict()
+
+    def __call__(self, epochs, cam=False, image_idx=None):
+        if cam and image_idx is None:
+            image_idx = [15]
+
+        for epoch in range(epochs):
+            self.train()
+            self.test()
+
+            if cam:
+                self.grad_cam(image_idx)
+
+            if self.scheduler_per_epoch:
+                self.scheduler.step()
+
+    def plot(self):
+        self.train.plot_stats()
+        self.test.plot_stats()
+
+    def grad_cam(self, image_idx):
+        if self.cam is None:
+            self.cam = CAM(
+                model=self.train.model,
+                target_layers=self.target_layers,
+                device=self.train.device,
+            )
+
+        dataset = self.test.testloader.dataset
+        mean = dataset.mean
+        std = dataset.std
+
+        for idx in image_idx:
+            image, label = dataset[idx]
+
+            input_tensor = (image * std + mean).unsqueeze(0)
+            visualization = self.cam(input_tensor, label)
+
+            if idx not in self.cam_results:
+                self.cam_results[idx] = [
+                    label,
+                    (input_tensor[0].permute(1, 2, 0) * 255).numpy().astype("uint8"),
+                    visualization,
+                ]
+
+            else:
+                self.cam_results[idx].append(visualization)
+
+    def save_grad_cam_gif(self, image_idx, output_directory="./cam_output/"):
+        # save CAM output as gifs
+        for idx in image_idx:
+            images = self.cam_results[idx]
+            label, images = (
+                self.train.trainloader.dataset.classes[images[0]],
+                images[1:],
+            )
+
+            if not os.path.exists(output_directory):
+                os.makedirs(output_directory)
+
+            image_path = os.path.join(output_directory, f"{label}.gif")
+            imageio.mimsave(image_path, images, duration=10 * len(images))
+
+
+class CAM:
+    def __init__(self, model, target_layers, device):
+        self.cam = GradCAM(
+            model=model,
+            target_layers=target_layers,
+            use_cuda=device == "cuda",
+        )
+
+    def __call__(self, input_tensor, label):
+        targets = [ClassifierOutputTarget(label)]
+
+        grayscale_cam = self.cam(
+            input_tensor=input_tensor,
+            targets=targets,
+            aug_smooth=True,
+            eigen_smooth=True,
+        )
+
+        input_tensor = input_tensor.permute(0, 2, 3, 1).numpy()
+        return show_cam_on_image(input_tensor, grayscale_cam[0, :], use_rgb=True)[0]
 
 
 # plot correct/incorrect predictions
 class PlotOutput:
-    def __init__(self, device: str):
+    def __init__(self, model, target_layers, device: str):
+        self.mean = None
+        self.std = None
+        self.cam = None
         self.result = None
+
         self.device = device
         self.title = (
             "Actual Label: {target} \n Predicted Label: {prediction} \n Score: {score}"
         )
 
+        self.model = model
+        self.target_layers = target_layers
+
     def reset(self):
         self.result = {"images": [], "target": [], "prediction": [], "confidence": []}
 
-    def run_prediction(self, model, data_loader, class_list, ptype="incorrect"):
+    def denormalise(self, tensor):
+        return tensor * self.std + self.mean
+
+    def run_prediction(self, data_loader, ptype="incorrect"):
         self.reset()
-        model.eval()
+        self.model.eval()
+
+        self.mean = data_loader.dataset.mean
+        self.std = data_loader.dataset.std
 
         with torch.no_grad():
             for (data, target) in tqdm(data_loader):
                 data, target = data.to(self.device), target.to(self.device)
-                prediction = model(data)
+                prediction = self.model(data)
                 prediction = F.softmax(prediction, dim=-1)
 
                 # filter out correct/incorrect results
@@ -125,16 +290,19 @@ class PlotOutput:
                 idx = idx.nonzero(as_tuple=True)[0].tolist()
 
                 self.result["images"].extend(list(data[idx].cpu()))
-                self.result["prediction"].extend(
-                    [class_list[v] for v in values[idx].tolist()]
-                )
-                self.result["target"].extend(
-                    [class_list[t] for t in target[idx].tolist()]
-                )
+                self.result["prediction"].extend(values[idx].tolist())
+                self.result["target"].extend(target[idx].tolist())
                 self.result["confidence"].extend(score[idx].tolist())
 
-    def plot(self, n: int, reverse: bool = True):
-        cmap = "gray" if self.result["images"][0].shape[0] == 1 else None
+    def plot(
+        self, n: int, class_list: List, reverse: bool = True, grad_cam: bool = False
+    ):
+        if grad_cam and self.cam is None:
+            self.cam = CAM(
+                model=self.model, target_layers=self.target_layers, device=self.device
+            )
+
+        cmap = "gray" if self.mean.shape[0] == 1 else None
 
         grid_size = (math.ceil(n / 4), 4)
         fig, axs = plt.subplots(grid_size[0], grid_size[1], figsize=(12, 8))
@@ -149,14 +317,23 @@ class PlotOutput:
             j = sorted_indices[i]
 
             image = self.result["images"][j]
+            image = self.denormalise(image)
             image = image.clip(0, 1)
-            image = image.squeeze(0) if cmap == "gray" else image.permute(1, 2, 0)
+
+            if not grad_cam:
+                image = image.squeeze(0) if cmap == "gray" else image.permute(1, 2, 0)
+
+            else:
+                # image.unsqueeze(0) shape [1, 3, 32, 32]
+                image = self.cam(
+                    input_tensor=image.unsqueeze(0), label=self.result["prediction"][j]
+                )
 
             ax.imshow(image, cmap=cmap)
 
             ititle = self.title.format(
-                target=self.result["target"][j],
-                prediction=self.result["prediction"][j],
+                target=class_list[self.result["target"][j]],
+                prediction=class_list[self.result["prediction"][j]],
                 score=round(self.result["confidence"][j], 2),
             )
             ax.set_title(ititle, fontsize=7)
