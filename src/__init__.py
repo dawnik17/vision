7245e73ddb861c531310ed34b38dbee83312542a
dawnik17 <dawnik17@gmail.com>
2023-07-30 16:31:47 +0530

    backward compatibility

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from src.models.david_page import DavidPageNet
+from src.models.mnist import MNIST8k, MNIST13k
+from src.models.resnet import (ResNet, ResNet18, ResNet34, ResNet50, ResNet101,
+                               ResNet152)
